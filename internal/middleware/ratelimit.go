02bf10f2352d406a244351876246152aab80117d
Q <qug2210@gmail.com>
2026-02-17 11:41:09 +0700

    Update API rate limit to 3000 req/min

--- internal/middleware/ratelimit.go
+++ internal/middleware/ratelimit.go
@@ -7,10 +7,10 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
-// LimitGlobal is a general rate limiter for the API (e.g. 100 req/min)
+// LimitGlobal is a general rate limiter for the API (e.g. 2000 req/min)
 func LimitGlobal() fiber.Handler {
 	return limiter.New(limiter.Config{
-		Max:        100,
+		Max:        3000,
 		Expiration: 1 * time.Minute,
 		KeyGenerator: func(c *fiber.Ctx) string {
 			return c.IP()
@@ -24,10 +24,10 @@ func LimitGlobal() fiber.Handler {
 }
 
 // LimitStrict is a strict rate limiter for sensitive actions (e.g. creating alias, login)
-// Default: 5 req/min
+// Default: 20 req/min
 func LimitStrict() fiber.Handler {
 	return limiter.New(limiter.Config{
-		Max:        5,
+		Max:        20,
 		Expiration: 1 * time.Minute,
 		KeyGenerator: func(c *fiber.Ctx) string {
 			return c.IP()
